78bdd6517a3cd0fa67aa0c7318cba7310c3da7bd
andantet <orrdaniel73@gmail.com>
2022-07-23 01:10:30 +0100

    Initial commit

---
+++ src/main/java/me/andante/noclip/api/NoClip.java
@@ -0,0 +1,10 @@
+package me.andante.noclip.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface NoClip {
+    String MOD_ID   = "noclip";
+    String MOD_NAME = "noclip";
+    Logger LOGGER   = LoggerFactory.getLogger(MOD_ID);
+}
---
+++ src/main/java/me/andante/noclip/impl/NoClipImpl.java
@@ -0,0 +1,10 @@
+package me.andante.noclip.impl;
+
+import me.andante.noclip.api.NoClip;
+import net.fabricmc.api.ModInitializer;
+
+public final class NoClipImpl implements NoClip, ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
