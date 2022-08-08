56a4cd51703767852934d1203c4148bc3a06e5d6
andantet <orrdaniel73@gmail.com>
2022-08-08 10:06:36 +0100

    setSneaking -> isInSneakingPose
    (cherry picked from commit 75295214a7d2af41c2a299701882088eadb1836d)

--- src/main/java/me/andante/noclip/mixin/EntityMixin.java
+++ src/main/java/me/andante/noclip/mixin/EntityMixin.java
@@ -4,16 +4,12 @@ import me.andante.noclip.impl.ClippingEntity;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-    @Shadow public abstract boolean hasVehicle();
-
     /**
      * Makes pistons and shulker boxes ignore clipping entities.
      */
@@ -35,13 +31,9 @@ public abstract class EntityMixin {
     /**
      * Cancels enabling sneak when clipping.
      */
-    @Inject(method = "setSneaking", at = @At("HEAD"), cancellable = true)
-    private void onSetSneaking(boolean sneaking, CallbackInfo ci) {
-        if (sneaking) {
-            if (this.hasVehicle()) return;
-
-            Entity that = (Entity) (Object) this;
-            if (that instanceof ClippingEntity clippingEntity && clippingEntity.isClipping()) ci.cancel();
-        }
+    @Inject(method = "isInSneakingPose", at = @At("HEAD"), cancellable = true)
+    private void onIsInSneakingPose(CallbackInfoReturnable<Boolean> cir) {
+        Entity that = (Entity) (Object) this;
+        if (that instanceof ClippingEntity clippingEntity && clippingEntity.isClipping()) cir.setReturnValue(false);
     }
 }
